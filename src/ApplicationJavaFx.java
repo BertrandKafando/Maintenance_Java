b7b266adc585d1f1c35a1fb1c5f00bdf29a2ba00
bertrand <bertrandkafando07@gmail.com>
2022-01-15 00:21:50 +0100

    responsablemain

--- src/ApplicationJavaFx.java
+++ src/ApplicationJavaFx.java
@@ -1,9 +1,17 @@
+package Presentation;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ApplicationJavaFx extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        AnchorPane pane= FXMLLoader.load(getClass().getResource("responsableMain.fxml"));
+        Scene sc=new Scene(pane,1400,700);
+        primaryStage.setScene(sc);
+        primaryStage.show();
     }
 }
