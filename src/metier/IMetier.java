db49f68954d0e8c9099e6479ab3a32e85b5cdb8d
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-01-15 00:38:26 +0100

    sprint

--- src/metier/IMetier.java
+++ src/metier/IMetier.java
@@ -6,6 +6,7 @@ public interface IMetier {
     void ajouterOrdreTravail(OrdreTravail ot);
     void modifierOrdreTravail(OrdreTravail ot);
     void supprimerOrdreTravail(OrdreTravail ot);
+
     List<OrdreTravail> getOrdreTravailIntervenant(Intervenant it);
     List<OrdreTravail> getAllOrdreTravail();
 
