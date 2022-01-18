8b4a002a914276d362ef9931d0e1d9502bc40a7b
bertrand <bertrandkafando07@gmail.com>
2022-01-18 22:37:50 +0100

    sprintt

--- src/Controller/ControllerCompteResponsable.java
+++ src/Controller/ControllerCompteResponsable.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CompteRenponsable implements Initializable {
+public class ControllerCompteResponsable implements Initializable {
 
     @FXML
     private TextField cnm;
